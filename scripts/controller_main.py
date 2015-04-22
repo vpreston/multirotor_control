95d645014ea5f7ff2db57355dd5b848534b3ec09
vpreston <victorialynnpreston@gmail.com>
2015-04-22 15:13:51 -0400

    First draft of full control loop

--- scripts/controller_main.py
+++ scripts/controller_main.py
@@ -35,6 +35,38 @@ class MCN():
         self.armed = False
         self.risen = False
         self.failsafe = False
+        #variables that need memory
+        self.xerror = 0
+        self.xerrorv = 0
+        self.xerrora = 0
+        self.oldxerror = 0
+        self.oldxerrorv = 0
+        self.yerror = 0
+        self.yerrorv = 0
+        self.yerrora = 0
+        self.oldyerror = 0
+        self.oldyerrorv = 0
+        self.zerror = 0
+        self.zerrorv = 0
+        self.zerrora = 0
+        self.oldzerror = 0
+        self.oldzerrorv = 0
+
+        self.psierror = 0
+        self.psierrv = 0
+        self.psierra = 0
+        self.oldpsierror = 0
+        self.oldpsierrv = 0
+        self.phierror = 0
+        self.phierrv = 0
+        self.phierra = 0
+        self.oldphierror = 0
+        self.oldphierrv = 0
+        self.thetaerror = 0
+        self.thetaerrv = 0
+        self.thetaerra = 0
+        self.oldthetaerror = 0
+        self.oldthetaerrv = 0
         #set up communications protocol
         self.pub_rc = rospy.Publisher('/send_rc', roscopter.msg.RC)
         self.sub_state = rospy.Subscriber('/state', State, self.state_check)
@@ -56,7 +88,7 @@ class MCN():
         self.jzz = (2*self.mq*self.r**2)/5 + 4*self.mm*self.l**2
         self.g = -9.8
         #controller
-        r = rospy.Rate(20)
+        r = rospy.Rate(20) # 0.1 second loop rate
         while not rospy.is_shutdown():
             self.hover_uncompensated()
             r.sleep()
@@ -86,6 +118,12 @@ class MCN():
         self.xacc = data.xacc
         self.yacc = data.yacc
         self.zacc = data.zacc
+        self.xgyro = data.xgyro
+        self.ygyro = data.ygyro
+        self.zgyro = data.zgyro
+        self.xmag = data.xmag
+        self.ymag = data.ymag
+        self.zmag = data.zmag
 
     def parse_attitude(self, data):
         #Reads in data of interest from the vehicle
@@ -93,7 +131,7 @@ class MCN():
         self.pitch = data.pitch
         self.yaw = data.yaw
         self.rollspeed = data.rollspeed
-        self.pitchspeed = data.pitchspeed 
+        self.pitchspeed = data.pitchspeed
         self.yawspeed data.yawspeed
 
     def hover_uncompensated(self):
@@ -146,17 +184,67 @@ class MCN():
 
     def hover_loop(self):
         #TODO Read in desired vector, convert to desired orientation
-        x = self.desire['x']
-        y = self.desire['y']
-        z = self.desire['z']
-        h = self.desire['h']
+        xd = self.desire['x']
+        yd = self.desire['y']
+        zd = self.desire['z']
+        hd = self.desire['h']
 
+        #Calculate desired pitch and roll orientations based upon acceleration of x and y
+
+        self.xerror = xd - self.xmag - self.xgyro
+        self.yerror = yd - self.ymag - self.ygyro
+        self.zerror = zd - self.zmag - self.zgyro
+        self.xerrorv = (self.xerror - self.oldxerror)*0.1
+        self.xerrora = (self.xerrorv - self.oldxerrorv)*0.1
+        self.yerrorv = (self.yerror - self.oldyerror)*0.1
+        self.yerrora = (self.yerrorv - self.oldyerrorv)*0.1
+        self.zerrorv = (self.zerror - self.oldzerror)*0.1
+        self.zerrora = (self.zerrorv - self.oldzerrorv)*0.1
+
+        pd = self.g*self.yerrora*np.cos(self.yaw)
+        td = -self.g*self.xerrora*np.cos(self.yaw)
+
+        self.phierror = pd - self.roll - self.rollspeed
+        self.psierror = hd - self.yaw - self.yawspeed
+        self.thetaerror = td - self.pitch - self.pitchspeed
+        self.phierrv = (self.phierror - self.oldphierror)*0.1
+        self.phierra = (self.phierrv - self.oldphierrv)*0.1
+        self.psierrv = (self.psierror - self.oldpsierror)*0.1
+        self.psierra = (self.psierrv - self.oldpsierrv)*0.1
+        self.thetaerrv = (self.thetaerror - self.oldthetaerror)*0.1
+        self.thetaerra =  (self.thetaerrv - self.oldthetaerrv)*0.1
+
+        phival = self.phierra * self.jxx/(self.l*self.bt)
+        psival = self.psierra * self.jzz/(self.bh)
+        thetaval = self.thetaerra * self.jyy/(self.l*self.bt)
+        zval = self.zerrora / (self.bt*(1/(self.mm+self.mq)) - self.g)
+
+        w4 = np.sqrt((zval - psival - phival)/4)
+        w1 = np.sqrt((psival - thetaval + phival + 2*w4**2)/2)
+        w2 = np.sqrt(phival + w4**2)
+        w3 = np.sqr(thetaval + w1**2)
+
+        sig1 = (w1 + 10)/0.08
+        sig2 = (w2 + 10)/0.08
+        sig3 = (w3 + 10)/0.08
+        sig4 = (w4 + 10)/0.08
+
+        self.oldxerror = self.xerror
+        self.oldyerror = self.yerror
+        self.oldzerror = self.zerror
         
+        self.oldxerrorv = self.xerrorv
+        self.oldyerrorv = self.yerrorv
+        self.oldzerrorv = self.zerrorv
 
+        self.oldphierror = self.phierror
+        self.oldpsierror = self.psierror
+        self.oldthetaerror = self.thetaerror
 
-
-
-
+        self.oldphierrv = self.phierrv
+        self.oldpsierrv = self.psierrv
+        self.oldthetaerrv = self.thetaerrv
+        
         #Reads in information about vehicle in order to determine action
         if self.buttons: #If button pressed
             if self.buttons[3]: #Disarm and plot data from flight
@@ -175,9 +263,7 @@ class MCN():
                 rospy.sleep(5)
 
         if self.armed and not self.failsafe:
-            #TODO Read in desired and take difference with reality
-            #TODO Convert to signal
-            #TODO Publish signal
+            (self.twist[0], self.twist[1], self.twist[2], self.twist[3]) = (int(sig1), int(sig2), int(sig3), int(sig4))
 
         elif self.failsafe:
             self.command_serv(2) #Sends the land command
